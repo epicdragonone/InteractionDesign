cc89e045c0c637058b1ff21872fdd152deeb3b5f
Akki465 <akkisingh04@hotmail.com>
2024-05-14 15:36:28 +0100

    filterPage initialisation

--- my_app/lib/filterPage.dart
+++ my_app/lib/filterPage.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class FiltersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filters'),
+      ),
+      body: Center(
+        child: Text('Filter options will go here'),
+      ),
+    );
+  }
+}
