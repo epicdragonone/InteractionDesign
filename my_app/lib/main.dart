f55cae12389c68f2fbc2a34e5dc26be016f7a44c
= <pelumi@babalola.com>
2024-05-16 14:23:54 +0100

    cragPage progress, +assets folder

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: const CragPage(title: 'TEST, DO NOT PUSH TO MAIN '),
     );
   }
 }
